3e7efb4c8733bd75c66f7aeab920b0b807d5c925
Sleepy1Ash <faustoperezortega@gmail.com>
2025-09-15 17:29:59 -0600

    feat: agregar páginas Home y Counter + Navbar compartido

---
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-page',
+  imports: [],
+  templateUrl: './counter-page.component.html',
+})
+export class CounterPageComponent {
+  counter = signal(10);
+  increaseBy(value: number = 1) {
+    this.counter.update((current) => current + value);
+  }
+  decreaseBy(value: number = 1) {
+    this.counter.update((current) => current - value);
+  }
+  constructor(){
+    const saved = localStorage.getItem('counter');
+    if (saved) {
+      this.counter.set(Number(saved));
+    }
+    effect(() => {
+      console.log('Counter changed: ', this.counter());
+      localStorage.setItem('counter', this.counter().toString());
+    });
+  }
+}
