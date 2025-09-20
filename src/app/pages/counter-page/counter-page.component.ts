7b24f422cd37d62744e2fb13e979189418f27bd4
Sleepy1Ash <faustoperezortega@gmail.com>
2025-09-20 10:37:55 -0600

    CorreccionCounter

--- src/app/pages/counter-page/counter-page.component.ts
+++ src/app/pages/counter-page/counter-page.component.ts
@@ -2,6 +2,7 @@ import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
+  standalone: true, 
   imports: [],
   templateUrl: './counter-page.component.html',
 })
